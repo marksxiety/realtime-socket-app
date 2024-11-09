36601c0e535438341c67f327025d3e437d96c00e
Mark Ciril <markciril.jamon@gmail.com>
2024-11-10 00:44:57 +0800

    Feat: Implement Socket.IO client communication for receiving messages
    Set up Vue component to listen for messages from server for clients
    
    Bind received messages to reactive variables for dynamic display
    
    Added socket event listeners to handle incoming messages and update UI

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="grid grid-cols-3 place-items-center gap-4 min-h-screen p-4">
+  <div class="grid grid-cols-3 place-items-center gap-4 min-h-screen p-24">
     <Card class="overflow-auto w-full min-h-40">
       <CardHeader class="space-y-1">
         <CardTitle class="text-2xl"> Client 1 </CardTitle>
       </CardHeader>
       <CardContent class="grid grid-cols-2 gap-4">
-        message recieved from ws here
+        <p>{{ client1Message }}</p>
       </CardContent>
     </Card>
 
@@ -14,7 +14,7 @@
         <CardTitle class="text-2xl"> Client 2 </CardTitle>
       </CardHeader>
       <CardContent class="grid grid-cols-2 gap-4">
-        message recieved from ws here
+        <p>{{ client2Message }}</p>
       </CardContent>
     </Card>
 
@@ -23,12 +23,46 @@
         <CardTitle class="text-2xl"> Client 3 </CardTitle>
       </CardHeader>
       <CardContent class="grid grid-cols-2 gap-4">
-        message recieved from ws here
+        <p>{{ client3Message }}</p>
       </CardContent>
     </Card>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Card, CardContent, CardFooter, CardHeader, CardTitle, } from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { io } from 'socket.io-client'
+
+const client1Message = ref('0')
+const client2Message = ref('0')
+const client3Message = ref('0')
+
+let socket: any
+onMounted(() => {
+
+  socket = io('http://localhost:8080')
+
+  socket.on('client1', (message: string) => {
+    console.log(`Received message from client1: ${message}`)
+    client1Message.value = message
+  })
+
+  socket.on('client2', (message: string) => {
+    console.log(`Received message from client2: ${message}`)
+    client2Message.value = message
+  })
+
+  socket.on('client3', (message: string) => {
+    console.log(`Received message from client3: ${message}`)
+    client3Message.value = message
+  })
+
+})
+
+onBeforeUnmount(() => {
+  if (socket) {
+    socket.disconnect()
+  }
+})
 </script>
